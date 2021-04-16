37fe49c3a230e3d25b4a78f5e2e102f8708a1e31
Thiago Ferrari <thiagofcjr@gmail.com>
2021-04-16 19:01:07 -0300

    revisaoProva 2021-04-16

---
+++ L5_classeInterface/src/main/java/Exe00/Cidadao.java
@@ -0,0 +1,8 @@
+package Exe00;
+
+public interface Cidadao {
+
+    public String tiraRG();
+
+    public String tiraCPF();
+}
---
+++ L5_classeInterface/src/main/java/Exe00/Eleitor.java
@@ -0,0 +1,10 @@
+package Exe00;
+
+public interface Eleitor {
+    // método de interface são abstratos e não tem codigo
+
+    public String tiraTitulo();
+
+    public String votar();
+
+}
